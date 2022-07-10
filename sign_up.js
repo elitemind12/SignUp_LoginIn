40f0fdc2029bd4d516b38a1f951819faf4083b50
Daniel Makyao <86465222+elitemind12@users.noreply.github.com>
2022-07-10 08:16:18 +0300

    changes

--- sign_up.js
+++ sign_up.js
@@ -10,10 +10,36 @@ Email.addEventListener('input', (e) => {
     if (EmailValue.match(emailFormat)) {
         emailSpan.innerText = 'Valid Email Address';
         emailSpan.classList.add('success');
-        return true;
     } else {
         emailSpan.innerText = 'Invalid Email Address';
         emailSpan.classList.add('danger');
-        return false;
+    }
+});
+
+Password.addEventListener('input', () => {
+    const passValue = Password.value;
+    const passSpan = document.getElementById('errorPass');
+    const passFormat = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{7,15}$/;
+
+    if (passValue.match(passFormat)) {
+        passSpan.innerText = 'Valid Password';
+        passSpan.classList.add('success');
+    } else {
+        passSpan.innerText = 'Invalid Password';
+        passSpan.classList.add('danger');
+    }
+});
+
+ConfirmPassword.addEventListener('input', () => {
+    const passValue = Password.value;
+    const confirmValue = ConfirmPassword.value;
+    const confirmSpan = document.getElementById('errorConfirm');
+
+    if (confirmValue.match(passValue)) {
+        confirmSpan.innerText = 'password Confirm';
+        confirmSpan.classList.add('success');
+    } else {
+        confirmSpan.innerText = 'password not match';
+        confirmSpan.classList.add('danger');
     }
 })
